c0f25ae1fcdf489a7a9bea6934a34e9cb12fe5ee
Nicolas Martin <nicolas.martin@arobas-music.com>
2014-04-25 17:22:41 +0200

    ajout d'une trace d'exemple

--- samples/main.cpp
+++ samples/main.cpp
@@ -5,28 +5,48 @@
 
 void f1(int n)
 {
-	set_thread_name("thread1");
+	set_thread_name("GUIThread");
+
+	{
+		auto _a = make_activity("Import");
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+	{
+		auto _a = make_activity("Engraving");
+		{
+			auto _a = make_activity("BuildingSceneGraph");
+			std::this_thread::sleep_for(std::chrono::milliseconds(42));
+		}
+		{
+			auto _a = make_activity("Collision");
+			std::this_thread::sleep_for(std::chrono::milliseconds(32));
+		}
+
+	}
+
 	for (int i = 0; i < 5; ++i) {
-		auto _a = make_activity("executing");
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		auto _b = make_activity("subexecuting");
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		auto _a = make_activity("Render");
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		{
+			auto _b = make_activity("painting");
+			std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+		}
 	}
 }
 
 void f2(int& n)
 {
-	set_thread_name("thread2");
+	set_thread_name("AudioThread");
 	{
-		auto _a = make_activity("loading");
+		auto _a = make_activity("loading Samples");
 		for (int i = 0; i < 5; ++i) {
 			add_mark((std::string("sample#") + std::to_string(i)).c_str());
 			std::this_thread::sleep_for(std::chrono::milliseconds(10 + 10*i));
 		}
 	}
-	for (int i = 0; i < 50; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		{
-			auto _a = make_activity("executing");
+			auto _a = make_activity("FillBuffer");
 			++n;
 			
 			std::this_thread::sleep_for(std::chrono::milliseconds(210));
